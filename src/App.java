5e966d9d316b20db494400b269d383fe3bc97324
정병천 <star1431@naver.com>
2025-08-18 12:59:47 +0900

    commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("멋사 백엔드 부트캠프 19기: Java");
     }
 }
---
+++ src/week_02/_0818/Exam.java
@@ -0,0 +1,11 @@
+package week_02._0818;
+
+public class Exam { // 클래스 선언 (파일명=클래스명)
+    public static void main(String[] args) { // main 메소드 선언 (프로그램 시작점)
+        System.out.println("TITLE : 0818 자바 기초"); // java api의 시스템 클래스, 콘솔출력
+        /**
+         * System.out : 콘솔
+         * System.in : 키보드
+         */
+    }
+}
