42cba4a90c82fbeb456d4f109f80446c8bdc91a3
정병천 <star1431@naver.com>
2025-08-19 11:55:28 +0900

    이전 정리내용 일부 수정 @commit

--- src/week_02/_0818/Exam.java
+++ src/week_02/_0818/Exam.java
@@ -6,10 +6,10 @@ package week_02._0818;
  */
 public class Exam { // 클래스 선언 (파일명=클래스명)
     public static void main(String[] args) { // main 메소드 선언 (프로그램 시작점)
-        System.out.println("\nTITLE : 0818 자바 기초"); // java api의 시스템 클래스, 콘솔출력
+        System.out.println("TITLE : 0818 자바 기초"); // java api의 시스템 클래스, 콘솔출력
         /*
-          System.out : 콘솔
-          System.in : 키보드
+         *  System.out : 콘솔
+         *  System.in : 키보드
          */
 
         // static 아닌 경우 객체 생성 후 호출임.
