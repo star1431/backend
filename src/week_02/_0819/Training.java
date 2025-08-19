38e7f81386d8ff18a254fa422a3f0a0347e8397e
정병천 <star1431@naver.com>
2025-08-19 16:02:55 +0900

    내용정리 및 실습 @commit

---
+++ src/week_02/_0819/Training.java
@@ -0,0 +1,62 @@
+package week_02._0819;
+import java.util.*;
+
+public class Training {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("===== 계산기 (+-*/%) =======");
+        System.out.println("입력방식 ex) 101 % 10");
+        System.out.print("값입력 : ");
+        String inputVal = input.nextLine();
+        input.close();
+
+        /*
+         * StringTokenizer(파라매터,"구분자식별", 리턴값)
+         */
+        StringTokenizer tokenStr = new StringTokenizer(inputVal, "+-*/%", true);
+
+        String preVal   ="0";
+        String optVal   = "";
+        String nextVal  = "0";
+
+        if (tokenStr.hasMoreTokens()) preVal  = tokenStr.nextToken().trim();
+        if (tokenStr.hasMoreTokens()) optVal  = tokenStr.nextToken().trim();
+        if (tokenStr.hasMoreTokens()) nextVal = tokenStr.nextToken().trim();
+
+        int preNum, nextNum;
+
+        long resultNum;
+
+        
+        preNum  = Integer.parseInt(preVal);
+        nextNum = Integer.parseInt(nextVal);
+        
+        String opt = optVal;
+
+        switch (opt) {
+            case "+":
+                resultNum = preNum + nextNum;
+                break;
+            case "-":
+                resultNum = preNum - nextNum;
+                break;
+            case "*":
+                resultNum = preNum * nextNum;
+                break;
+            case "/":
+                resultNum = preNum / nextNum;
+                break;
+            case "%":
+                resultNum = preNum % nextNum;
+                break;
+            default:
+                resultNum = 0;
+                break;
+        }
+        System.out.println();
+        System.out.printf("입력값은 \'%s\' 이고,\n", inputVal);
+        System.out.printf("결과값은 \'%d\' 입니다.\n", resultNum);
+        System.out.println("=========== END ============");
+    }
+}
