45d2d683e3ca397ce8b56734d6f15394f75055b0
정병천 <star1431@naver.com>
2025-09-24 13:15:25 +0900

    @commit

--- src/week_07/_0922/Main.java
+++ src/week_07/_0922/Main.java
@@ -1,22 +1,22 @@
 package week_07._0922;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int year = 543;
-        while (true) {
-            int input = sc.nextInt();
-            if(input <= 1000 || input >= 3000) {
-                System.out.println("1001 ~ 3000 까지 입력");
-            } else {
-                System.out.println((input - year));
-                sc.close();
-                break;
-            }
+        Scanner sc =new Scanner(System.in);
+
+        String a = sc.nextLine().trim();
+        String[] strArr;
+
+        strArr = a.split(" ");
+
+        long sum = 0;
+        for(String item: strArr){
+            sum += Long.parseLong(item);
         }
+
+        System.out.println(sum);
     }
 }
