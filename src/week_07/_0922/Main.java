6e952e5c46ef6cf1bb8d0d9ae2ffa9fe809819d2
정병천 <star1431@naver.com>
2025-09-25 14:19:51 +0900

    @commit

--- src/week_07/_0922/Main.java
+++ src/week_07/_0922/Main.java
@@ -4,19 +4,27 @@ package week_07._0922;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int hh = sc.nextInt();
+        int mm = sc.nextInt();
 
-        String a = sc.nextLine().trim();
-        String[] strArr;
+        final int MAX_HOUR = 24;
+        final int MAX_MIN = 60;
+        int pointMin = 45;
 
-        strArr = a.split(" ");
+        boolean check = mm >= pointMin;
+        int setHour, setMin;
 
-        long sum = 0;
-        for(String item: strArr){
-            sum += Long.parseLong(item);
+        if(check) {
+            setMin = mm - pointMin;
+            setHour = hh;
+        } else {
+            setMin = MAX_MIN - pointMin + mm;
+            setHour = (hh == 0) ? MAX_HOUR - 1 : hh - 1;
         }
 
-        System.out.println(sum);
+        System.out.println(setHour + " " + setMin);
     }
 }
