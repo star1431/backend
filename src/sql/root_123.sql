f91bed6e3b86c2e3386d8ef8a940354eef25cf94
정병천 <star1431@naver.com>
2025-10-29 18:21:59 +0900

    @commit

--- src/sql/root_123.sql
+++ src/sql/root_123.sql
@@ -6,3 +6,23 @@ SHOW GRANTS FOR 'star1431'@'%';
 
 SHOW DATABASES LIKE 'miniprojdb';
 FLUSH PRIVILEGES;
+
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'dwarf123ab@';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'dwarf123ab@';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'dwarf123ab@';
+FLUSH PRIVILEGES;
+
+
+SELECT user, host, plugin
+FROM mysql.user
+WHERE user = 'root';
+
+commit;
+ALTER USER 'root'@'%' ACCOUNT LOCK;
+FLUSH PRIVILEGES;
+
+SELECT user, host, account_locked
+FROM mysql.user
+WHERE user='root';
